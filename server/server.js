4e85581209cab64a56f146c052b98898874a3a51
Manini21 <maninichhabra@gmail.com>
2025-08-31 23:36:31 +0530

    Update server.js file

--- server/server.js
+++ server/server.js
@@ -1,31 +1,25 @@
-// server.js
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-require('dotenv').config();
-const authRoutes = require('./routes/auth');
-
 const app = express();
 
+require('dotenv').config();
+
 // Middleware
-app.use(express.json());
 app.use(cors());
+app.use(express.json()); // Allows us to get data from the body
 
 // Connect to MongoDB
-const dbURI = process.env.MONGO_URI;
-
-mongoose.connect(dbURI, {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
-  useUnifiedTopology: true
-}).then(() => console.log('MongoDB connected'))
-  .catch(err => console.log('MongoDB connection error:', err));
+  useUnifiedTopology: true,
+})
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
 
-// Routes
-app.use('/api/auth', authRoutes);
+// Define routes
+app.use('/api/auth', require('./routes/auth'));
 
 // Start the server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
-console.log('JWT_SECRET:', process.env.JWT_SECRET);
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server is running on port ${port}`));
